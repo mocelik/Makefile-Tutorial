fd6c1e8b3fe79048dd2533e129c74795de750f47
Muhammed O. Celik <morhancelik@gmail.com>
2024-04-20 11:15:09 -0400

    Essentials - More Automatic Variables

--- 3-make-essentials/2-automatic-variables.mk
+++ 3-make-essentials/2-automatic-variables.mk
@@ -1,10 +1,13 @@
 
 # $@, $< and $^ are by far the most commonly used automatic variables
+target: prereq3
 target: prereq1 prereq2
-	@echo Target Name:  $@
-	@echo First prereq: $<
-	@echo All prereqs:  $^
+	@echo "Target Name:            $@"
+	@echo "First prereq:           $<"
+	@echo "All unique prereqs:     $^"
+	@echo "All duplicated prereqs: $+"
 
+target: prereq4
 target: prereq3
 
 # Create empty recipes for prereq*
@@ -23,6 +26,17 @@ wild-%:
 # The rule will run once per every target requested
 lion tiger:
 	@echo Hello $@
+
+# Add D to get the directory of an automatic variable
+# Add F to get the filename of an automatic variable
+# Note that these variables need parentheses or braces
+dir1/dir2/file1.txt: dir1/dir2/prereq1 dir1/dir2/prereq2 dir1/dir2/dir3
+	@echo "Directory: ${@D}"
+	@echo "Filename:  ${@F}"
+	@echo "Directory: ${^D}"
+	@echo "Filename:  ${^F}"
+
+dir1/dir2/%: ;
 
 # Refer to the following page for more on automatic variables
 # https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html
