523f2b0668e636e96ae9a822ccb4078606fd01a7
Muhammed O. Celik <morhancelik@gmail.com>
2024-04-20 12:39:22 -0400

    Essentials - Mentioned braces in logging

--- 3-make-essentials/3-logging.mk
+++ 3-make-essentials/3-logging.mk
@@ -12,4 +12,5 @@ error:
 	$(info You can't see me!)
 
 $(info Even though this info line is at the end of the makefile, it gets printed first. Why?)
+${info Note that you can also use braces instead of parentheses}
 $(info )
