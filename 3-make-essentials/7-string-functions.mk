10ac989fbab7ec4ff400b33944b866f62fb8ed51
Muhammed O. Celik <morhancelik@gmail.com>
2024-04-21 00:20:39 -0400

    Essentials - String functions formatting

--- 3-make-essentials/7-string-functions.mk
+++ 3-make-essentials/7-string-functions.mk
@@ -4,23 +4,23 @@ do_nothing_target:;@:
 ################################################################################
 
 REGIONS := africa asia europe
-$(info REGIONS = [${REGIONS}])
-
-REGIONS_C := $(addsuffix .c, ${REGIONS})
-$(info $$(addsuffix .c,[list]): [${REGIONS_C}])
+$(info  REGIONS =                   [${REGIONS}])
+$(info $$(addsuffix .c,[list]):     [$(addsuffix .c, ${REGIONS})])
 $(info $$(addprefix hello/,[list]): [$(addprefix hello/,${REGIONS})])
-# addsuffix .c: africa.c asia.c europe.c
+# addsuffix .c:     africa.c asia.c europe.c
 # addprefix hello/: hello/africa hello/asia hello/europe
 
 $(info )
 
-### Partial Substitutions ###
+### Pattern Substitutions ###
 ################################################################################
 
 # These all replace the .c with .o in REGIONS_C
+REGIONS_C := $(addsuffix .c, ${REGIONS})
+$(info  REGIONS_C =                 [${REGIONS_C}])
 $(info $$(patsubst %.c,%.o,[list]): [$(patsubst %.c,%.o,${REGIONS_C})])
-$(info $$([list]:%.c=%.o): [${REGIONS_C:%.c=%.o}])
-$(info $$([list]:.c=.o): [${REGIONS_C:.c=.o}])
+$(info $${[list]:%.c=%.o}:          [${REGIONS_C:%.c=%.o}])
+$(info $${[list]:.c=.o}:            [${REGIONS_C:.c=.o}])
 # africa.o asia.o europe.o
 
 $(info )
@@ -29,11 +29,9 @@ $(info )
 ################################################################################
 
 SRC_FILES := dir1/hello.c dir1/world.c dir2/steel.c dir2/foundations.c
-DIR1_SRC_FILES := $(filter dir1/%,${SRC_FILES})
-DIR2_SRC_FILES := $(filter-out dir1/%,${SRC_FILES})
-$(info SRC_FILES: [${SRC_FILES}])
-$(info filter dir1: [$(filter dir1/%,${SRC_FILES})])
-$(info filter-out dir1: [$(filter-out dir1/%,${SRC_FILES})])
+$(info  SRC_FILES:                 [${SRC_FILES}])
+$(info $$(filter dir1,[list]):     [$(filter     dir1/%,${SRC_FILES})])
+$(info $$(filter-out dir1,[list]): [$(filter-out dir1/%,${SRC_FILES})])
 # filter dir1: dir1/hello.c dir1/world.c
 # filter-out dir1: dir2/steel.c dir2/foundations.c
 
