151dae049ce8f0592d4593da79d2465cdcfb65ce
Muhammed O. Celik <morhancelik@gmail.com>
2024-04-21 11:38:57 -0400

    Essentials - simplified foreach

--- 3-make-essentials/6-foreach-function.mk
+++ 3-make-essentials/6-foreach-function.mk
@@ -1,14 +1,11 @@
 
 all: hello-all
 
-# Recall multi-target rules...
-africa asia:
-	@echo Hello $@
-
 # syntax: $(foreach variable-name,[list],expression-using-variable)
 REGIONS := africa asia europe
 HELLO-REGIONS := $(foreach REGION,${REGIONS},hello-${REGION})
 $(info HELLO-REGIONS is: [${HELLO-REGIONS}])
+# HELLO-REGIONS is: [hello-africa hello-asia hello-europe]
 
 # hello-all: hello-africa hello-asia hello-europe
 hello-all: ${HELLO-REGIONS}
