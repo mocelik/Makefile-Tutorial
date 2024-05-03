b3cf4a1d3e9a539cb7f715992112e45a073e28d5
Muhammed O. Celik <morhancelik@gmail.com>
2024-05-03 00:41:03 -0400

    Advanced: Cleaned implicit rules

--- 4-make-advanced/implicit-rules.mk
+++ 4-make-advanced/implicit-rules.mk
@@ -1,20 +1,27 @@
 
 $(info Try running make with --debug=i to see implicit debug info)
 $(info Also try running with make --no-builtin-rules)
+$(info )
 
 ### Implicit Rule and Rule Chaining ###
 ################################################################################
 
 # Default target is steel, but where is the recipe that builds it?
-# That recipe is built-in to make; it is explicit.
-steel:
+# That recipe is built-in to make; it is implicit.
+.PHONY: all
+all: steel foundations
+
+# By explicitly stating these file names, they will not be considered
+# intermediate files anymore, and will persist if created by a built-in rule
+steel: steel.o
+steel.o: steel.c
 
 %.c:
-	printf "#include <stdio.h>\n\r\
-	int main(void) {\n\r\
-		printf(\"HelloWorld from $@\");\n\r\
-		return 0;\n\r\
-	}\n\r" > $@
+	printf "#include <stdio.h>\n\
+	int main(void) {\n\
+		printf(\"HelloWorld from $@\");\n\
+		return 0;\n\
+	}\n" > $@
 
 clean:
-	rm -f steel *.c
+	rm -f steel foundations *.o *.c
