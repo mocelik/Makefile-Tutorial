48fae3e19a9334a00298ef0715dbf035ba91c9a8
Muhammed O. Celik <morhancelik@gmail.com>
2024-06-26 00:12:27 -0400

    5.3 formatting and cxx support

--- 5-build-system/3-modular/executable.mk
+++ 5-build-system/3-modular/executable.mk
@@ -1,4 +1,7 @@
 
+### Variables ###
+################################################################################
+
 ifeq (${BUILD_DIR},)
 $(warning BUILD_DIR not set or empty, defaulting to build)
 BUILD_DIR := build
@@ -9,21 +12,30 @@ $(warning BIN_DIR not set or empty, defaulting to ${BUILD_DIR}/bin)
 BIN_DIR := ${BUILD_DIR}/bin
 endif
 
+CDEPS_FLAGS := -MMD -MP
+
+### Functions ###
+################################################################################
+
 # $1 -> appname
 # Uses:
 # 	CSRCS_${appname}
 # 	CFLAGS_${appname}
+# 	CPPFLAGS_${appname}
 # 	LDFLAGS_${appname}
 # 	LDLIBS_${appname}
 define add_executable_target=
 $(eval COBJS_$1 := $(sort \
 	$(addprefix ${BUILD_DIR}/,$(patsubst %.c,%.o,${CSRCS_$1}))))
-$(call internal_add_executable_target,$1)
-endef
 
-# $1 -> app name
-# $2 -> object files for app
-define internal_add_executable_target=
+$(eval CXXOBJS_$1 := $(sort \
+	$(addprefix ${BUILD_DIR}/,\
+	$(patsubst %.cpp,%.o,$(filter %.cpp,${CXXSRCS_$1})))))
+
+$(eval CXXOBJS_$1 += $(sort \
+	$(addprefix ${BUILD_DIR}/,\
+	$(patsubst %.cc,%.o,$(filter %.cc,${CXXSRCS_$1})))))
+
 all: $1
 .PHONY: $1
 $1: ${BIN_DIR}/$1
@@ -42,9 +54,5 @@ $(sort $(dir ${COBJS_$1}) ${BIN_DIR}):
 .PHONY: clean-$1
 clean: clean-$1
 clean-$1:
-	${RM} -r ${BUILD_DIR}/$1 ${COBJS_$1}
-
+	${RM} -r ${BIN_DIR}/$1 ${COBJS_$1}
 endef
-
-### Internal Variables ###
-CDEPS_FLAGS := -MMD -MP
