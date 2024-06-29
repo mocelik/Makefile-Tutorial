33d88437fd9fa7a0ce6162ab18e881725f598520
Muhammed O. Celik <morhancelik@gmail.com>
2024-06-29 14:27:35 -0400

    Added comments to executable func

--- 5-build-system/3-modular/executable.mk
+++ 5-build-system/3-modular/executable.mk
@@ -28,21 +28,28 @@ define add_c_executable_target=
 $(eval COBJS_$1 := $(sort \
 	$(addprefix ${BUILD_DIR}/,$(patsubst %.c,%.o,${CSRCS_$1}))))
 
+# Add this target to all
 all: $1
 .PHONY: $1
 $1: ${BIN_DIR}/$1
+
+# Rule to Link C
 ${BIN_DIR}/$1: ${COBJS_$1} | ${BIN_DIR}
 	${CC} ${LDFLAGS_$1} $$^ -o $$@ ${LDLIBS_$1}
 
+# Rule to Compile C
 ${COBJS_$1}: ${BUILD_DIR}/%.o : %.c
 	${CC} ${CDEPS_FLAGS} ${CFLAGS_$1} ${CPPFLAGS_$1} -c $$< -o $$@
 
+# Rule to create build directory mirror
 $(foreach OBJ_FILE,${COBJS_$1},$(eval ${OBJ_FILE}: | $(dir ${OBJ_FILE})))
 $(sort $(dir ${COBJS_$1}) ${BIN_DIR}):
 	mkdir -p $$@
 
+# Rules (auto-generated) for object dependencies
 -include $(patsubst %.o,%.d,${COBJS_$1})
 
+# Rules to clean
 .PHONY: clean-$1
 clean-$1:
 	${RM} -r ${BIN_DIR}/$1 ${COBJS_$1}
